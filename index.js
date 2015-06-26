fcf53cc400de7ae55265c3dae6177a40c9493ace
Adrian Sieber <mail@adriansieber.com>
2015-06-26 21:55:35 +0200

    Add simple test which prints the image to the command line

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
 import {rasterize} from '../source/index.js'
 
-console.log(rasterize({
+console.time('test')
+
+let pixels = rasterize({
 	shapes: [
 		{
-			type: 'circle',
-			radius: 2,
-			center: {
-				x: 5,
-				y: 5
-			}
+			type: 'rectangle',
+			width: 4,
+			height: 4,
+			topLeftCorner: {
+				x: 2,
+				y: 2
+			},
+			color: 0x00ff00
 		}
 	],
 	viewport: {
-		height: 10,
-		width: 10
+		height: 20,
+		width: 20
 	}
-}))
+})
 
-console.log(rasterize())
+console.timeEnd('test')
+
+console.log(
+	pixels
+		.map(row => {
+			return '| ' +
+			row
+			.map(pixel => {
+				return (pixel === 0) ? '  ' : '█ '
+			})
+			.join('') +
+			'|'
+		})
+		.join('\n')
+)
