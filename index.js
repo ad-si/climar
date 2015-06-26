979193dcab2166303a92c56b8bf734888c82f1a2
Adrian Sieber <mail@adriansieber.com>
2015-06-26 23:46:18 +0200

    Render more shapes in test, improve formatting

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import {rasterize} from '../source/index.js'
 import {render} from '../test/cliRenderer.js'
 
-console.time('test')
 
-let pixels = rasterize({
+let options = {
 	shapes: [
+		{
+			type: 'circle',
+			radius: 2,
+			center: {
+				x: 5,
+				y: 5
+			}
+		},
 		{
 			type: 'rectangle',
 			width: 4,
@@ -14,13 +21,38 @@ let pixels = rasterize({
 				y: 2
 			},
 			color: 0x00ff00
+		},
+		{
+			type: 'rectangle',
+			width: 5,
+			height: 5,
+			topLeftCorner: {
+				x: 7,
+				y: 7
+			},
+			color: 0x00ff00
+		},
+		{
+			type: 'rectangle',
+			width: 15,
+			height: 1,
+			topLeftCorner: {
+				x: 2,
+				y: 15
+			},
+			color: 0x00ff00
 		}
 	],
 	viewport: {
 		height: 20,
 		width: 20
 	}
-})
+}
+
+
+console.time('test')
+
+let pixels = rasterize(options)
 
 console.timeEnd('test')
 
