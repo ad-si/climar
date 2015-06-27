1ecab76c791054f0ec148e0ecc55e287db3f118a
Adrian Sieber <mail@adriansieber.com>
2015-06-27 16:58:44 +0200

    Add wide rendering mode for scaled image rendering

--- climar.js
+++ climar.js
@@ -61,6 +61,132 @@ function stringifyPixels (
 		offset + bottomBorder
 }
 
+function createHighResolutionPixelString (
+		{
+			pixels = [],
+			style = 'wide'
+		} = {}
+	) {
+
+	let height = pixels.length,
+		width = pixels[0].length,
+
+		// Always generate an image where width and height are even
+		// so that the image size aligns with the unicode block elements
+		evenHeight = isEven(height) ? height : height + 1,
+		evenWidth = isEven(width) ? width : width + 1,
+
+		pixelsScaled = new Array(evenHeight / 2)
+			.fill()
+			.map(row =>
+				new Array(evenWidth / 2).fill(0)
+			),
+		narrowCharacters = {
+			' ' : ' ',
+			'▗' : '▗',
+			'▖' : '▖',
+			'▄' : '▄',
+			'▝' : '▝',
+			'▐' : '▐',
+			'▞' : '▞',
+			'▟' : '▟',
+			'▘' : '▘',
+			'▚' : '▚',
+			'▌' : '▌',
+			'▙' : '▙',
+			'▀' : '▀',
+			'▜' : '▜',
+			'▛' : '▛',
+			'█' : '█'
+		},
+		wideCharacters = {
+			' ' : '  ',
+			'▗' : ' ▄',
+			'▖' : '▄ ',
+			'▄' : '▄▄',
+			'▝' : ' ▀',
+			'▐' : ' █',
+			'▞' : '▄▀',
+			'▟' : '▄█',
+			'▘' : '▀ ',
+			'▚' : '▀▄',
+			'▌' : '█ ',
+			'▙' : '█▄',
+			'▀' : '▀▀',
+			'▜' : '▀█',
+			'▛' : '█▀',
+			'█' : '██'
+		},
+		characterSet = wideCharacters,
+		joinCharacter = ''
+
+
+	if (style === 'narrow') {
+		characterSet = narrowCharacters
+		joinCharacter = ' '
+	}
+
+	for (let y = 0; y < evenHeight; y += 2) {
+		for (let x = 0; x < evenWidth; x += 2) {
+
+				let character = ' '
+
+				if (pixels[y][x] === 0)
+					if (pixels[y][x + 1] === 0)
+						if (pixels[y + 1][x] === 0)
+							if (pixels[y + 1][x + 1] === 0)
+								character = characterSet[' ']
+							else
+								character = characterSet['▗']
+						else
+							if (pixels[y + 1][x + 1] === 0)
+								character = characterSet['▖']
+							else
+								character = characterSet['▄']
+					else
+						if (pixels[y + 1][x] === 0)
+							if (pixels[y + 1][x + 1] === 0)
+								character = characterSet['▝']
+							else
+								character = characterSet['▐']
+						else
+							if (pixels[y + 1][x + 1] === 0)
+								character = characterSet['▞']
+							else
+								character = characterSet['▟']
+				else
+					if (pixels[y][x + 1] === 0)
+						if (pixels[y + 1][x] === 0)
+							if (pixels[y + 1][x + 1] === 0)
+								character = characterSet['▘']
+							else
+								character = characterSet['▚']
+						else
+							if (pixels[y + 1][x + 1] === 0)
+								character = characterSet['▌']
+							else
+								character = characterSet['▙']
+					else
+						if (pixels[y + 1][x] === 0)
+							if (pixels[y + 1][x + 1] === 0)
+								character = characterSet['▀']
+							else
+								character = characterSet['▜']
+						else
+							if (pixels[y + 1][x + 1] === 0)
+								character = characterSet['▛']
+							else
+								character = characterSet['█']
+
+			pixelsScaled[y/2][x/2] = character
+		}
+	}
+
+	return pixelsScaled
+		.map(row => `| ${row.join(joinCharacter)}|`)
+		.join('\n')
+}
+
 export function render (
 		{
 			pixels = [],
@@ -72,79 +198,6 @@ export function render (
 		return stringifyPixels({pixels})
 	}
 	else if (scale === 0.5) {
-
-		let height = pixels.length,
-			width = pixels[0].length,
-
-			// Always generate an image where width and height are even
-			// so that the image size aligns with the unicode block elements
-			evenHeight = isEven(height) ? height : height + 1,
-			evenWidth = isEven(width) ? width : width + 1,
-
-			pixelsScaled = new Array(evenHeight / 2)
-				.fill()
-				.map(row =>
-					new Array(evenWidth / 2).fill(0)
-				)
-
-		for (let y = 0; y < evenHeight; y += 2) {
-			for (let x = 0; x < evenWidth; x += 2) {
-
-					let character = ' '
-
-					if (pixels[y][x] === 0)
-						if (pixels[y][x + 1] === 0)
-							if (pixels[y + 1][x] === 0)
-								if (pixels[y + 1][x + 1] === 0)
-									character = ' '
-								else
-									character = '▗'
-							else
-								if (pixels[y + 1][x + 1] === 0)
-									character = '▖'
-								else
-									character = '▄'
-						else
-							if (pixels[y + 1][x] === 0)
-								if (pixels[y + 1][x + 1] === 0)
-									character = '▝'
-								else
-									character = '▐'
-							else
-								if (pixels[y + 1][x + 1] === 0)
-									character = '▞'
-								else
-									character = '▟'
-					else
-						if (pixels[y][x + 1] === 0)
-							if (pixels[y + 1][x] === 0)
-								if (pixels[y + 1][x + 1] === 0)
-									character = '▘'
-								else
-									character = '▚'
-							else
-								if (pixels[y + 1][x + 1] === 0)
-									character = '▌'
-								else
-									character = '▙'
-						else
-							if (pixels[y + 1][x] === 0)
-								if (pixels[y + 1][x + 1] === 0)
-									character = '▀'
-								else
-									character = '▜'
-							else
-								if (pixels[y + 1][x + 1] === 0)
-									character = '▛'
-								else
-									character = '█'
-
-				pixelsScaled[y/2][x/2] = character
-			}
-		}
-
-		return pixelsScaled
-			.map(row => `| ${row.join(' ')}|`)
-			.join('\n')
+		return createHighResolutionPixelString({pixels})
 	}
 }
