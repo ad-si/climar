74d39acfb1f3a6a750dc845e02cdace9d78181ea
Adrian Sieber <mail@adriansieber.com>
2015-06-27 11:31:17 +0200

    Improve formatting of test

--- index.js
+++ index.js
@@ -3,60 +3,62 @@ import {render} from '../test/cliRenderer.js'
 
 
 let options = {
-	shapes: [
-		{
-			type: 'circle',
-			radius: 2,
-			center: {
-				x: 5,
-				y: 5
+		shapes: [
+			{
+				type: 'circle',
+				radius: 2,
+				center: {
+					x: 5,
+					y: 5
+				}
+			},
+			{
+				type: 'rectangle',
+				width: 4,
+				height: 4,
+				topLeftCorner: {
+					x: 2,
+					y: 2
+				},
+				color: 0x00ff00
+			},
+			{
+				type: 'rectangle',
+				width: 5,
+				height: 5,
+				topLeftCorner: {
+					x: 7,
+					y: 7
+				},
+				color: 0x00ff00
+			},
+			{
+				type: 'rectangle',
+				width: 15,
+				height: 1,
+				topLeftCorner: {
+					x: 2,
+					y: 15
+				},
+				color: 0x00ff00
 			}
-		},
-		{
-			type: 'rectangle',
-			width: 4,
-			height: 4,
-			topLeftCorner: {
-				x: 2,
-				y: 2
-			},
-			color: 0x00ff00
-		},
-		{
-			type: 'rectangle',
-			width: 5,
-			height: 5,
-			topLeftCorner: {
-				x: 7,
-				y: 7
-			},
-			color: 0x00ff00
-		},
-		{
-			type: 'rectangle',
-			width: 15,
-			height: 1,
-			topLeftCorner: {
-				x: 2,
-				y: 15
-			},
-			color: 0x00ff00
+		],
+		viewport: {
+			height: 20,
+			width: 20
 		}
-	],
-	viewport: {
-		height: 20,
-		width: 20
-	}
-}
+	},
+	timerDescription = 'Rasterization Time'
 
 
-console.time('test')
+console.time(timerDescription)
 
 let pixels = rasterize(options)
 
-console.timeEnd('test')
+console.timeEnd(timerDescription)
 
 
+console.log()
 console.log(render({
 	pixels,
 	scale: 0.5
