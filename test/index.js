12296b04b7d76ad96941dc44b692678dd3dcd653
Adrian Sieber <mail@adriansieber.com>
2015-06-27 15:44:29 +0200

    Remove obsolete code from tests

--- test/index.js
+++ test/index.js
@@ -1,65 +1,13 @@
-import {rasterize} from '../source/index.js'
-import {render} from '../test/cliRenderer.js'
+import {render} from '../climar'
 
+let pixels = [
+	[0xffffffff, 0, 0, 0],
+	[0, 0xffffffff, 0, 0],
+	[0, 0, 0xffffffff, 0],
+	[0, 0, 0, 0xffffffff]
+]
 
-let options = {
-		shapes: [
-			{
-				type: 'circle',
-				radius: 2,
-				center: {
-					x: 5,
-					y: 5
-				}
-			},
-			{
-				type: 'rectangle',
-				width: 4,
-				height: 4,
-				topLeftCorner: {
-					x: 2,
-					y: 2
-				},
-				color: 0x00ff00
-			},
-			{
-				type: 'rectangle',
-				width: 5,
-				height: 5,
-				topLeftCorner: {
-					x: 7,
-					y: 7
-				},
-				color: 0x00ff00
-			},
-			{
-				type: 'rectangle',
-				width: 15,
-				height: 1,
-				topLeftCorner: {
-					x: 2,
-					y: 15
-				},
-				color: 0x00ff00
-			}
-		],
-		viewport: {
-			height: 20,
-			width: 20
-		}
-	},
-	timerDescription = 'Rasterization Time'
-
-
-console.time(timerDescription)
-
-let pixels = rasterize(options)
-
-console.timeEnd(timerDescription)
-
-
-console.log()
 console.log(render({
 	pixels,
-	scale: 0.5
+	scale: 1
 }))
