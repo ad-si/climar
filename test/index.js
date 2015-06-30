a07582b51a3f390e8fd7e0e73776b98611826db2
Adrian Sieber <mail@adriansieber.com>
2015-06-30 23:39:24 +0200

    Add test for image with semi-transparent pixels

--- test/index.js
+++ test/index.js
@@ -28,4 +28,28 @@ describe('Climar', () => {
 			'    --------\n'
 		)
 	})
+
+	it('Renders a 4x4 black, transparent & semi-transparent image', () => {
+		let pixels = [
+			[0xffffffff, 0, 0, 0],
+			[0, 0xffffffbb, 0, 0],
+			[0, 0, 0xffffff66, 0],
+			[0, 0, 0, 0xffffffff11]
+		]
+
+		expect(render({
+			pixels,
+			scale: 1,
+			colorize: false
+		}))
+		.to.equal(
+			'    0 1 2 3\n' +
+			'    ________\n' +
+			'0  |█▌      |\n' +
+			'1  |  ▓▌    |\n' +
+			'2  |    ▒▌  |\n' +
+			'3  |      ░▌|\n' +
+			'    --------\n'
+		)
+	})
 })
