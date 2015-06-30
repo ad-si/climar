817d9590cc18e6b45f4cca6cfeec12d6eb1208de
Adrian Sieber <mail@adriansieber.com>
2015-06-30 21:02:16 +0200

    Use mocha and chai for testing, test rendering of a 4x4 bw image

--- test/index.js
+++ test/index.js
@@ -1,13 +1,31 @@
 import {render} from '../climar'
+import mocha from 'mocha'
+import {expect} from 'chai'
 
-let pixels = [
-	[0xffffffff, 0, 0, 0],
-	[0, 0xffffffff, 0, 0],
-	[0, 0, 0xffffffff, 0],
-	[0, 0, 0, 0xffffffff]
-]
 
-console.log(render({
-	pixels,
-	scale: 1
-}))
+mocha.describe('Climar', () => {
+
+	it('Renders a 4x4 black & white image', () => {
+		let pixels = [
+			[0xffffffff, 0, 0, 0],
+			[0, 0xffffffff, 0, 0],
+			[0, 0, 0xffffffff, 0],
+			[0, 0, 0, 0xffffffff]
+		]
+
+		expect(render({
+			pixels,
+			scale: 1,
+			colorize: false
+		}))
+		.to.equal(
+			'    0 1 2 3\n' +
+			'    ________\n' +
+			'0  |█▌      |\n' +
+			'1  |  █▌    |\n' +
+			'2  |    █▌  |\n' +
+			'3  |      █▌|\n' +
+			'    --------\n'
+		)
+	})
+})
